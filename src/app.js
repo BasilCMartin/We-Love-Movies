ac951ed2649aa1bad0c9c3302664793e8a571db0
BasilCMartin <imconnormartin@gmail.com>
2022-08-03 14:06:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.set('port', PORT)
+
 
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
