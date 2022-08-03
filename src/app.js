1e7ce2499fbb19a0f495372781f00cd3b5dc538b
BasilCMartin <imconnormartin@gmail.com>
2022-08-03 12:22:07 -0400

    added app.set port

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.set('port', PORT)
+
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
 const theatersRouter = require("./theaters/theaters.router")
